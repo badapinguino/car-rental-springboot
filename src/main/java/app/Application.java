28794c67afc5dac20980c5a74e9f02bb5ddb1b36
badapinguino <55546000+badapinguino@users.noreply.github.com>
2019-10-29 11:11:47 +0100

    Project finished. Removed one unused import.

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,7 +6,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
